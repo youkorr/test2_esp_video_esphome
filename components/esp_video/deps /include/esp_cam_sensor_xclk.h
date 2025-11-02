bf9371b7d37c5295c397cd3c5951edd8ef481b59
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-02 23:50:59 +0100

    Create esp_cam_sensor_xclk.h

---
+++ components/esp_video/deps /include/esp_cam_sensor_xclk.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdint.h>
+#include "esp_err.h"
+#include "driver/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct esp_cam_sensor_xclk_config_t {
+    gpio_num_t xclk_pin;
+    uint32_t xclk_freq_hz;
+} esp_cam_sensor_xclk_config_t;
+
+esp_err_t esp_cam_sensor_xclk_init(esp_cam_sensor_xclk_config_t *config);
+esp_err_t esp_cam_sensor_xclk_deinit(void);
+
+#ifdef __cplusplus
+}
+#endif
