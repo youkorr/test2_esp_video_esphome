67f3a5b28286aa669ccf22fc4de32f1fea593ed8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-03 04:29:58 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -11,34 +11,42 @@ namespace lvgl_camera_display {
 
 class LVGLCameraDisplay : public Component {
  public:
+  // ---- Méthodes ESPHome ----
   void setup() override;
   void loop() override;
   void dump_config() override;
 
+  // ---- Liaison caméra / LVGL ----
   void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
   void configure_canvas(lv_obj_t *canvas);
 
+  // ---- Compatibilité ESPHome YAML ----
+  void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
+  void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
+
   float get_setup_priority() const override { return setup_priority::LATE; }
 
-  void stop_task();  // pour arrêter proprement la tâche (optionnel)
+  // ---- Contrôle manuel de la tâche (optionnel) ----
+  void stop_task();
 
  protected:
-  // Objets liés
+  // ---- Objets liés ----
   mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
+  std::string canvas_id_{};
 
-  // État
+  // ---- États internes ----
   bool first_update_{true};
   bool stop_task_flag_{false};
 
-  // Stats
-  uint32_t update_interval_{33};
+  // ---- Paramètres et stats ----
+  uint32_t update_interval_{33};  // ms, pour compatibilité YAML
   uint32_t frame_count_{0};
 
-  // FreeRTOS
+  // ---- FreeRTOS ----
   TaskHandle_t camera_task_handle_{nullptr};
 
-  // Méthodes internes
+  // ---- Méthodes internes ----
   static void camera_task_trampoline_(void *param);
   void camera_task_();
   void update_canvas_();
@@ -46,6 +54,7 @@ class LVGLCameraDisplay : public Component {
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
+
 
 
 
