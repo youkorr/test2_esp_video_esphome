25e46c1850d669b0945f95987fe40b02f2d18479
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-03 04:51:08 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -3,52 +3,38 @@
 #include "esphome/core/component.h"
 #include "esphome/components/lvgl/lvgl_esphome.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 namespace esphome {
 namespace lvgl_camera_display {
 
 class LVGLCameraDisplay : public Component {
  public:
-  // ---- Méthodes ESPHome ----
   void setup() override;
   void loop() override;
   void dump_config() override;
 
-  // ---- Liaison caméra / LVGL ----
   void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
-  void configure_canvas(lv_obj_t *canvas);
-
-  // ---- Compatibilité ESPHome YAML ----
   void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
   void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
 
+  void configure_canvas(lv_obj_t *canvas);
+
   float get_setup_priority() const override { return setup_priority::LATE; }
 
-  // ---- Contrôle manuel de la tâche (optionnel) ----
-  void stop_task();
-
  protected:
-  // ---- Objets liés ----
   mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
   std::string canvas_id_{};
 
-  // ---- États internes ----
-  bool first_update_{true};
-  bool stop_task_flag_{false};
+  uint32_t update_interval_{33};
+  uint32_t last_update_{0};
 
-  // ---- Paramètres et stats ----
-  uint32_t update_interval_{33};  // ms, pour compatibilité YAML
   uint32_t frame_count_{0};
+  bool first_update_{true};
+  bool canvas_warning_shown_{false};
 
-  // ---- FreeRTOS ----
-  TaskHandle_t camera_task_handle_{nullptr};
+  uint32_t last_fps_time_{0};
 
-  // ---- Méthodes internes ----
-  static void camera_task_trampoline_(void *param);
-  void camera_task_();
   void update_canvas_();
 };
 
