4175150fc22b587e831706e79c93c033603f50f6
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-03 04:27:56 +0100

    Update lvgl_camera_display.h

--- components/lvgl_camera_display/lvgl_camera_display.h
+++ components/lvgl_camera_display/lvgl_camera_display.h
@@ -3,6 +3,8 @@
 #include "esphome/core/component.h"
 #include "esphome/components/lvgl/lvgl_esphome.h"
 #include "../mipi_dsi_cam/mipi_dsi_cam.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 namespace esphome {
 namespace lvgl_camera_display {
@@ -13,41 +15,37 @@ class LVGLCameraDisplay : public Component {
   void loop() override;
   void dump_config() override;
 
-  // Liaison avec la caméra MIPI
   void set_camera(mipi_dsi_cam::MipiDsiCam *camera) { this->camera_ = camera; }
-
-  // Configuration du canvas LVGL (à appeler depuis YAML ou code)
   void configure_canvas(lv_obj_t *canvas);
-
-  // Paramètres optionnels
-  void set_canvas_id(const std::string &canvas_id) { this->canvas_id_ = canvas_id; }
-  void set_update_interval(uint32_t interval_ms) { this->update_interval_ = interval_ms; }
 
   float get_setup_priority() const override { return setup_priority::LATE; }
 
+  void stop_task();  // pour arrêter proprement la tâche (optionnel)
+
  protected:
-  // --- Objets liés ---
+  // Objets liés
   mipi_dsi_cam::MipiDsiCam *camera_{nullptr};
   lv_obj_t *canvas_obj_{nullptr};
-  std::string canvas_id_{};
 
-  // --- Gestion du temps / FPS ---
-  uint32_t update_interval_{33};  // Intervalle théorique (ms)
-  uint32_t last_update_{0};
+  // État
+  bool first_update_{true};
+  bool stop_task_flag_{false};
+
+  // Stats
+  uint32_t update_interval_{33};
   uint32_t frame_count_{0};
 
-  // --- États internes ---
-  bool first_update_{true};
-  bool canvas_warning_shown_{false};
+  // FreeRTOS
+  TaskHandle_t camera_task_handle_{nullptr};
 
-  // --- Buffer LVGL interne ---
-  uint8_t *lv_buffer_{nullptr};
-
-  // --- Fonctions internes ---
+  // Méthodes internes
+  static void camera_task_trampoline_(void *param);
+  void camera_task_();
   void update_canvas_();
 };
 
 }  // namespace lvgl_camera_display
 }  // namespace esphome
+
 
 
