b331e02441c16e571c2532a1c34957c462057d38
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-04 17:43:54 +0100

    Create mipi_dsi_cam.h

---
+++ components/depot_test/mipi_dsi_cam/mipi_dsi_cam.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/i2c/i2c.h"
+#include <string>
+
+#ifdef USE_ESP32_VARIANT_ESP32P4
+extern "C" {
+  #include "esp_cam_ctlr.h"
+  #include "esp_cam_ctlr_csi.h"
+  #include "driver/isp.h"
+  #include "esp_ldo_regulator.h"
+}
+#endif
+
+namespace esphome {
+namespace mipi_dsi_cam {
+
+enum PixelFormat {
+  PIXEL_FORMAT_RGB565 = 0,
+  PIXEL_FORMAT_YUV422 = 1,
+  PIXEL_FORMAT_RAW8 = 2,
+  PIXEL_FORMAT_RGB888 = 3,
+  PIXEL_FORMAT_BGR888 = 4,
+  PIXEL_FORMAT_JPEG = 5,
+  PIXEL_FORMAT_H264 = 6,
+};
+
+class ISensorDriver {
+public:
+  virtual ~ISensorDriver() = default;
+  
+  virtual const char* get_name() const = 0;
+  virtual uint16_t get_pid() const = 0;
+  virtual uint8_t get_i2c_address() const = 0;
+  virtual uint8_t get_lane_count() const = 0;
+  virtual uint8_t get_bayer_pattern() const = 0;
+  virtual uint16_t get_lane_bitrate_mbps() const = 0;
+  virtual uint16_t get_width() const = 0;
+  virtual uint16_t get_height() const = 0;
+  virtual uint8_t get_fps() const = 0;
+  
+  virtual esp_err_t init() = 0;
+  virtual esp_err_t read_id(uint16_t* pid) = 0;
+  virtual esp_err_t start_stream() = 0;
+  virtual esp_err_t stop_stream() = 0;
+  virtual esp_err_t set_gain(uint32_t gain_index) = 0;
+  virtual esp_err_t set_exposure(uint32_t exposure) = 0;
+  virtual esp_err_t write_register(uint16_t reg, uint8_t value) = 0;
+  virtual esp_err_t read_register(uint16_t reg, uint8_t* value) = 0;
+};
+
+class MipiDsiCam : public Component, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
+
+  void set_name(const std::string &name) { this->name_ = name; }
+  void set_external_clock_pin(uint8_t pin) { this->external_clock_pin_ = pin; }
+  void set_external_clock_frequency(uint32_t freq) { this->external_clock_frequency_ = freq; }
+  void set_reset_pin(GPIOPin *pin) { this->reset_pin_ = pin; }
+  void set_sensor_type(const std::string &type) { this->sensor_type_ = type; }
+  void set_sensor_address(uint8_t addr) { this->sensor_address_ = addr; }
+  void set_lane_count(uint8_t lanes) { this->lane_count_ = lanes; }
+  void set_bayer_pattern(uint8_t pattern) { this->bayer_pattern_ = pattern; }
+  void set_lane_bitrate(uint16_t mbps) { this->lane_bitrate_mbps_ = mbps; }
+  void set_resolution(uint16_t w, uint16_t h) { this->width_ = w; this->height_ = h; }
+  void set_pixel_format(PixelFormat format) { this->pixel_format_ = format; }
+  void set_jpeg_quality(uint8_t quality) { this->jpeg_quality_ = quality; }
+  void set_framerate(uint8_t fps) { this->framerate_ = fps; }
+  void set_red_gain(float gain) { this->red_gain_ = gain; }
+  void set_green_gain(float gain) { this->green_gain_ = gain; }
+  void set_blue_gain(float gain) { this->blue_gain_ = gain; }
+  void set_gain(uint8_t gain) { this->gain_ = gain; }
+
+  bool capture_frame();
+  bool start_streaming();
+  bool stop_streaming();
+  bool is_streaming() const { return this->streaming_; }
+  
+  uint8_t* get_image_data() { return this->current_frame_buffer_; }
+  size_t get_image_size() const { return this->frame_buffer_size_; }
+  uint16_t get_image_width() const { return this->width_; }
+  uint16_t get_image_height() const { return this->height_; }
+  PixelFormat get_pixel_format() const { return this->pixel_format_; }
+
+ protected:
+  uint8_t external_clock_pin_{36};
+  uint32_t external_clock_frequency_{24000000};
+  GPIOPin *reset_pin_{nullptr};
+  
+  std::string sensor_type_{""};
+  uint8_t sensor_address_{0x36};
+  uint8_t lane_count_{1};
+  uint8_t bayer_pattern_{3};
+  uint16_t lane_bitrate_mbps_{576};
+  uint16_t width_{1280};
+  uint16_t height_{720};
+  
+  std::string name_{"MIPI Camera"};
+  PixelFormat pixel_format_{PIXEL_FORMAT_RGB565};
+  uint8_t jpeg_quality_{10};
+  uint8_t framerate_{30};
+  float red_gain_{1.0};
+  float green_gain_{1.0};
+  float blue_gain_{1.0};
+  uint8_t gain_{90};
+  
+  // Gains précalculés en virgule fixe (x256) pour éviter les float dans l'ISR
+  uint32_t red_gain_fixed_{256};
+  uint32_t green_gain_fixed_{256};
+  uint32_t blue_gain_fixed_{256};
+
+  bool initialized_{false};
+  bool streaming_{false};
+  bool frame_ready_{false};
+  
+  uint32_t total_frames_received_{0};
+  uint32_t last_frame_log_time_{0};
+  
+  uint8_t *frame_buffers_[2]{nullptr, nullptr};
+  uint8_t *current_frame_buffer_{nullptr};
+  size_t frame_buffer_size_{0};
+  uint8_t buffer_index_{0};
+  
+  ISensorDriver *sensor_driver_{nullptr};
+  
+#ifdef USE_ESP32_VARIANT_ESP32P4
+  esp_cam_ctlr_handle_t csi_handle_{nullptr};
+  isp_proc_handle_t isp_handle_{nullptr};
+  esp_ldo_channel_handle_t ldo_handle_{nullptr};
+  
+  bool create_sensor_driver_();
+  bool init_sensor_();
+  bool init_ldo_();
+  bool init_csi_();
+  bool init_isp_();
+  bool allocate_buffer_();
+  void apply_color_gains_(uint8_t* buffer);
+  
+  static bool IRAM_ATTR on_csi_new_frame_(
+    esp_cam_ctlr_handle_t handle,
+    esp_cam_ctlr_trans_t *trans,
+    void *user_data
+  );
+  
+  static bool IRAM_ATTR on_csi_frame_done_(
+    esp_cam_ctlr_handle_t handle,
+    esp_cam_ctlr_trans_t *trans,
+    void *user_data
+  );
+#endif
+};
+
+}  // namespace mipi_dsi_cam
+}  // namespace esphome
+
+
