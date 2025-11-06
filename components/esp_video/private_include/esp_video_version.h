d0cfd5f334f2f086c112461749efc14b5e328529
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-06 06:37:00 +0100

    Create esp_video_version.h

---
+++ components/esp_video/private_include/esp_video_version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @file esp_video_version.h
+ * @brief Définit la version du composant esp_video.
+ *
+ * Ces macros sont utilisées dans esp_video_ioctl.c pour renseigner
+ * la structure v4l2_capability (champ version).
+ */
+
+#define ESP_VIDEO_VER_MAJOR 1
+#define ESP_VIDEO_VER_MINOR 0
+#define ESP_VIDEO_VER_PATCH 0
