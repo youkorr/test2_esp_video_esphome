2a50e1b88afb1b0dd8ed9d00b5e7a8969c5669bd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-22 20:38:25 +0100

    Update __init__.py

--- components/video_player/__init__.py
+++ components/video_player/__init__.py
@@ -1,61 +1,52 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
 from esphome.const import (
     CONF_ID,
 )
 
 DEPENDENCIES = ["lvgl"]
-AUTO_LOAD = []
+
 CODEOWNERS = ["@youkorr"]
 
 video_player_ns = cg.esphome_ns.namespace("video_player")
 VideoPlayer = video_player_ns.class_("VideoPlayer", cg.Component)
 
-# ----- Keys -----
 CONF_SOURCE = "source"
-CONF_DEVICE = "device"
 CONF_WIDTH = "width"
 CONF_HEIGHT = "height"
 CONF_AUTOPLAY = "autoplay"
 CONF_LOOP = "loop"
+CONF_DEVICE = "device"
 
-# ----- Schema -----
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(VideoPlayer),
 
-    # chemin du fichier vidéo (ex: /sdcard/test.h264)
+    # Path to video file (local file or HTTP stream)
     cv.Required(CONF_SOURCE): cv.string,
 
-    # device H.264 (ex: /dev/video30)
-    cv.Required(CONF_DEVICE): cv.string,
+    # Output resolution of the video
+    cv.Optional(CONF_WIDTH, default=800): cv.positive_int,
+    cv.Optional(CONF_HEIGHT, default=480): cv.positive_int,
 
-    # résolution (doit rester cohérente avec la vidéo)
-    cv.Required(CONF_WIDTH): cv.positive_int,
-    cv.Required(CONF_HEIGHT): cv.positive_int,
+    # Auto-start playback
+    cv.Optional(CONF_AUTOPLAY, default=True): cv.boolean,
 
-    # flags optionnels
-    cv.Optional(CONF_AUTOPLAY, default=False): cv.boolean,
+    # Loop playback
     cv.Optional(CONF_LOOP, default=False): cv.boolean,
 
+    # H.264 hardware decoder device path (esp_video)
+    cv.Optional(CONF_DEVICE, default="/dev/video30"): cv.string,
 }).extend(cv.COMPONENT_SCHEMA)
 
 
-# ----- to_code() -----
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+
     await cg.register_component(var, config)
 
-    # Chemin source (H.264 file)
     cg.add(var.set_source_path(config[CONF_SOURCE]))
-
-    # Chemin du device H.264 (/dev/videoXX)
-    cg.add(var.set_device_path(config[CONF_DEVICE]))
-
-    # Resolution
-    cg.add(var.set_width(config[CONF_WIDTH]))
-    cg.add(var.set_height(config[CONF_HEIGHT]))
-
-    # Autoplay et boucle
+    cg.add(var.set_resolution(config[CONF_WIDTH], config[CONF_HEIGHT]))
     cg.add(var.set_autoplay(config[CONF_AUTOPLAY]))
     cg.add(var.set_loop(config[CONF_LOOP]))
+    cg.add(var.set_device_path(config[CONF_DEVICE]))
+
