955b009508bf949e6b16cc5e2ab51cb0244db609
youkorr <96953496+youkorr@users.noreply.github.com>
2025-11-26 01:24:06 +0100

    Update __init__.py

--- components/video_player/__init__.py
+++ components/video_player/__init__.py
@@ -1,100 +1,100 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.const import CONF_ID
 from esphome import automation
+from esphome.components import speaker
 
 DEPENDENCIES = ["lvgl"]
-
 CODEOWNERS = ["@youkorr"]
 
-# ESP-IDF component dependency
-ESP_IDF_COMPONENTS = ["esp_h264"]
-
-video_player_ns = cg.esphome_ns.namespace("video_player")
-VideoPlayer = video_player_ns.class_("VideoPlayer", cg.Component)
+simple_video_player_ns = cg.esphome_ns.namespace("simple_video_player")
+SimpleVideoPlayer = simple_video_player_ns.class_("SimpleVideoPlayer", cg.Component)
 
 # Actions
-PlayAction = video_player_ns.class_("PlayAction", automation.Action)
-PauseAction = video_player_ns.class_("PauseAction", automation.Action)
-StopAction = video_player_ns.class_("StopAction", automation.Action)
+PlayAction = simple_video_player_ns.class_("PlayAction", automation.Action)
+PauseAction = simple_video_player_ns.class_("PauseAction", automation.Action)
+StopAction = simple_video_player_ns.class_("StopAction", automation.Action)
+ResumeAction = simple_video_player_ns.class_("ResumeAction", automation.Action)
 
-CONF_SOURCE = "source"
+CONF_FILE_PATH = "file_path"
 CONF_WIDTH = "width"
 CONF_HEIGHT = "height"
-CONF_AUTOPLAY = "autoplay"
+CONF_BUFFER_SIZE = "buffer_size"
+CONF_AUTO_PLAY = "auto_play"
 CONF_LOOP = "loop"
-CONF_DEVICE = "device"
+CONF_SHOW_CONTROLS = "show_controls"
 CONF_PARENT_ID = "parent_id"
-
-# Get LVGL obj type
-lvgl_ns = cg.esphome_ns.namespace("lvgl")
-LvglComponent = lvgl_ns.class_("LvglComponent", cg.PollingComponent)
+CONF_SPEAKER = "speaker"
+CONF_FPS = "fps"
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(VideoPlayer),
-
-    # Path to video file (local file or HTTP stream)
-    cv.Required(CONF_SOURCE): cv.string,
-
-    # Output resolution of the video
+    cv.GenerateID(): cv.declare_id(SimpleVideoPlayer),
+    cv.Required(CONF_FILE_PATH): cv.string,
     cv.Optional(CONF_WIDTH, default=800): cv.positive_int,
     cv.Optional(CONF_HEIGHT, default=480): cv.positive_int,
-
-    # Auto-start playback
-    cv.Optional(CONF_AUTOPLAY, default=True): cv.boolean,
-
-    # Loop playback
-    cv.Optional(CONF_LOOP, default=False): cv.boolean,
-
-    # H.264 hardware decoder device path (esp_video)
-    cv.Optional(CONF_DEVICE, default="/dev/video30"): cv.string,
-
-    # Parent LVGL object (page/container) - if not set, uses current screen
+    cv.Optional(CONF_BUFFER_SIZE, default=200000): cv.positive_int,
+    cv.Optional(CONF_AUTO_PLAY, default=True): cv.boolean,
+    cv.Optional(CONF_LOOP, default=True): cv.boolean,
+    cv.Optional(CONF_SHOW_CONTROLS, default=False): cv.boolean,
     cv.Optional(CONF_PARENT_ID): cv.use_id(cg.void),
+    cv.Optional(CONF_SPEAKER): cv.use_id(speaker.Speaker),
+    cv.Optional(CONF_FPS): cv.positive_float,
 }).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
     await cg.register_component(var, config)
 
-    cg.add(var.set_source_path(config[CONF_SOURCE]))
-    cg.add(var.set_resolution(config[CONF_WIDTH], config[CONF_HEIGHT]))
-    cg.add(var.set_autoplay(config[CONF_AUTOPLAY]))
+    cg.add(var.set_file_path(config[CONF_FILE_PATH]))
+    cg.add(var.set_width(config[CONF_WIDTH]))
+    cg.add(var.set_height(config[CONF_HEIGHT]))
+    cg.add(var.set_buffer_size(config[CONF_BUFFER_SIZE]))
+    cg.add(var.set_auto_play(config[CONF_AUTO_PLAY]))
     cg.add(var.set_loop(config[CONF_LOOP]))
-    cg.add(var.set_device_path(config[CONF_DEVICE]))
+    cg.add(var.set_show_controls(config[CONF_SHOW_CONTROLS]))
+
+    if CONF_FPS in config:
+        cg.add(var.set_fps(config[CONF_FPS]))
 
     if CONF_PARENT_ID in config:
         parent = await cg.get_variable(config[CONF_PARENT_ID])
-        # Access underlying lv_obj_t* - LvPageType stores it in 'obj' member
         cg.add(var.set_parent(parent.obj))
+
+    if CONF_SPEAKER in config:
+        spk = await cg.get_variable(config[CONF_SPEAKER])
+        cg.add(var.set_speaker(spk))
 
 
 # Action schemas
-VIDEO_PLAYER_ACTION_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(VideoPlayer),
+SIMPLE_VIDEO_PLAYER_ACTION_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.use_id(SimpleVideoPlayer),
 })
 
 
-@automation.register_action("video_player.play", PlayAction, VIDEO_PLAYER_ACTION_SCHEMA)
+@automation.register_action("simple_video_player.play", PlayAction, SIMPLE_VIDEO_PLAYER_ACTION_SCHEMA)
 async def play_action_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
 
 
-@automation.register_action("video_player.pause", PauseAction, VIDEO_PLAYER_ACTION_SCHEMA)
+@automation.register_action("simple_video_player.pause", PauseAction, SIMPLE_VIDEO_PLAYER_ACTION_SCHEMA)
 async def pause_action_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
 
 
-@automation.register_action("video_player.stop", StopAction, VIDEO_PLAYER_ACTION_SCHEMA)
+@automation.register_action("simple_video_player.stop", StopAction, SIMPLE_VIDEO_PLAYER_ACTION_SCHEMA)
 async def stop_action_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    return var
+
+
+@automation.register_action("simple_video_player.resume", ResumeAction, SIMPLE_VIDEO_PLAYER_ACTION_SCHEMA)
+async def resume_action_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
