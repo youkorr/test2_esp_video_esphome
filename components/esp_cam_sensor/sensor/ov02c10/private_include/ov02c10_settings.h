105c2e89ff53d1ecc7c9dd07a6458d716abec463
youkorr <96953496+youkorr@users.noreply.github.com>
2025-12-17 18:43:42 +0100

    Update ov02c10_settings.h

--- components/esp_cam_sensor/sensor/ov02c10/private_include/ov02c10_settings.h
+++ components/esp_cam_sensor/sensor/ov02c10/private_include/ov02c10_settings.h
@@ -1312,9 +1312,9 @@ static const ov02c10_reginfo_t ov02c10_input_24M_MIPI_1lane_raw10_480x640_30fps_
     {0x3806, 0x04},  // Y end high ((60+976-1=1035) >> 8 = 4)
     {0x3807, 0x0b},  // Y end low (1035 & 0xFF = 0x0b = 11)
     // Output size: 480x640 (portrait orientation)
-    {0x3808, 0x01},  // width high
+    {0x3808, 0x02},  // width high
     {0x3809, 0xe0},  // width low = 480
-    {0x380a, 0x02},  // height high
+    {0x380a, 0x01},  // height high
     {0x380b, 0x80},  // height low = 640
     // Timing: SAME AS NATIVE
     {0x380c, 0x08},  // HTS high = 2280
